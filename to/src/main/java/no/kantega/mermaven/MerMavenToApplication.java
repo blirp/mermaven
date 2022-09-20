551d8351da1334a0f2576a443acd9076cf1b68b0
Martin Larsson <larma@runbox.no>
2022-09-20 22:47:18 +0200

    To -> springboot

---
+++ to/src/main/java/no/kantega/mermaven/MerMavenToApplication.java
@@ -0,0 +1,13 @@
+package no.kantega.mermaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerMavenToApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MerMavenToApplication.class, args);
+  }
+
+}
